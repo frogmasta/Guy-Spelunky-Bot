3f4ecdb122612b2dab53250d80723d6bb6e41990
frogmasta <ecrsoccerdude@gmail.com>
2021-05-07 00:50:33 -0500

    Changed mossranking to no downloading (doesn't decrease preformace enough to warrant downloading stuff). Also fixed reddit.py vulnerability.

--- cogs/mossranking.py
+++ cogs/mossranking.py
@@ -1,21 +1,12 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 from discord.ext import commands
-import discord
 from src.leaderboard import Leaderboard
-from datetime import datetime
-import json
-import os
-from pathlib import Path
 
+categories = {'main': "https://mossranking.com/ranking.php?id_ranking=18",
+              'speed': "https://mossranking.com/ranking.php?id_ranking=20",
+              'score': "https://mossranking.com/ranking.php?id_ranking=21"}
 
-def c(t):
-    ttime = datetime.strptime(t, '%m/%d/%y')
-    return ((datetime.now() - ttime).days)
-
-categories = {'main': "https://mossranking.com/ranking.php?id_ranking=18", 
-            'speed': "https://mossranking.com/ranking.php?id_ranking=20", 
-            'score': "https://mossranking.com/ranking.php?id_ranking=21"}
 
 class MossRanking(commands.Cog):
     def __init__(self, bot):
@@ -27,37 +18,20 @@ class MossRanking(commands.Cog):
             args = 'main'
         else:
             args = ' '.join(args).lower()
+
         if args not in categories:
             await ctx.send('not a valid category')
             return
-    
-        download_leaderboard(args)
-        
-        with open(f'mossranking_data/{args}.json', "r") as read_file:
-            data = json.load(read_file)
+
+        data = get_data(args)
 
         menu = Leaderboard(f"MossRanking - {args.capitalize()}", "", data)
         await menu.start(ctx)
 
-def download_leaderboard(category):
-    file_path = f'mossranking_data/{category}.json'
 
-    if not Path(file_path).is_file() or old_file(file_path):
-        print('downloading file...')
-        data = getData(category)
-        with open(file_path, 'w') as fout:
-            json.dump(data , fout)
-    return "Success"
-
-def old_file(file_path):
-    last_modified = datetime.utcfromtimestamp(os.path.getmtime(file_path))
-    now = datetime.utcnow()
-    return True if (now-last_modified).days >= 1 else False
-
-def getData(category):
-
+def get_data(category):
     page = requests.get(categories[category])
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser', from_encoding="iso-8859-1")
 
     table = soup.find('table', class_='table table-striped table-nonfluid')
     tableElements = table.findAll('tr')
@@ -70,7 +44,6 @@ def getData(category):
 
             name = elemData[1].find('span').get_text()
             points = elemData[2].get_text()
-            charImg = 'https://mossranking.com' + elemData[3].find('img').get('src')
             userRef = 'https://mossranking.com' + elemData[4].find('a').get('href')
 
             userData.append({'Name': name, 'Points': points, 'Details': f'[Profile]({userRef})'})
@@ -78,6 +51,7 @@ def getData(category):
             pass
 
     return userData
+
 
 def setup(bot):
     bot.add_cog(MossRanking(bot))
--- cogs/reddit.py
+++ cogs/reddit.py
@@ -1,62 +1,67 @@
-
-from discord.ext import commands
+import os
 import discord
 import asyncpraw
-from discord.ext import menus
+from discord.ext import commands, menus
+from dotenv import load_dotenv
+
+load_dotenv()
+REDDIT_TOKEN = os.getenv('REDDIT_TOKEN')
 
 categories = ['hot', 'new', 'top']
 
-async def createEmbed(description, image, name, link, curPage, p, icon, logo):
+
+async def createEmbed(description, image, name, link, curPage, icon, logo):
     if len(description) >= 2045:
         description = description[:2045] + '...'
     if len(name) >= 120:
         name = name[:120] + '...'
-    em = discord.Embed (
-            description = description,
-            color = 0xD2691E,
+
+    em = discord.Embed(
+        description=description,
+        color=0xD2691E,
     )
-    em.set_author(icon_url = logo, name=name, url=link)
+
+    em.set_author(icon_url=logo, name=name, url=link)
     em.set_footer(text=f'Page {curPage}/{25}', icon_url=icon)
     em.set_image(url=image)
     return em
 
-async def getPosts(subreddit, category, icon):
 
+async def getPosts(subreddit, category, icon):
     reddit = asyncpraw.Reddit(
-            client_id="HYreCcGUQx9hIQ",
-            client_secret="B4geGqB0zHjG4UQ5ZGHGJK7KWnSF-g",
-            user_agent="noah",
-        )
+        client_id="HYreCcGUQx9hIQ",
+        client_secret=REDDIT_TOKEN,
+        user_agent="noah",
+    )
 
     if subreddit.startswith(('/r/', 'r/')):
         subreddit = subreddit.split('r/')[-1]
 
-    
-    try: # Check if sub exists
-        if subreddit in ['all', 'popular']: # Special sub
+    try:  # Check if sub exists
+        if subreddit in ['all', 'popular']:  # Special sub
             sub = await reddit.subreddit(subreddit)
         else:
             sub = await reddit.subreddit(subreddit, fetch=True)
-    except: # it doesnt exist...
+    except:  # it doesnt exist...
         return None
 
-    if 'community_icon' not in dir(sub): # Special sub
+    if 'community_icon' not in dir(sub):  # Special sub
         logo = 'https://www.redditinc.com/assets/images/site/reddit-logo.png'
     else:
         if sub.community_icon != "":
             logo = sub.community_icon
         elif sub.icon_img != "":
             logo = sub.icon_img
-        else: # no logo
+        else:  # no logo
             logo = 'https://www.redditinc.com/assets/images/site/reddit-logo.png'
-        
 
+    posts = None
     if category == 'hot':
-        posts =  sub.hot(limit=25)
+        posts = sub.hot(limit=25)
     if category == 'new':
-        posts =  sub.new(limit=25)
+        posts = sub.new(limit=25)
     if category == 'top':
-        posts =  sub.top(limit=25)
+        posts = sub.top(limit=25)
 
     ems = []
     curPage = 0
@@ -65,97 +70,76 @@ async def getPosts(subreddit, category, icon):
         name = p.title
         link = "https://www.reddit.com" + p.permalink
         curPage += 1
-        
-        if postType == 'hosted:video': # video, cant rlly embed
+
+        if postType == 'hosted:video':  # video, cant rlly embed
             linkThumbnail = p.preview['images'][0]['source']['url']
-            
+
             # For another day
             # url = p.media['reddit_video']['fallback_url']
             # url = url.split("?")[0]
             # audio = url.split("DASH_")[0] + 'DASH_audio.mp4'
-            #ems.append(await createEmbed(url + ' ' + audio, linkThumbnail, name, link, curPage, p))
+            # ems.append(await createEmbed(url + ' ' + audio, linkThumbnail, name, link, curPage, p))
 
-            ems.append(await createEmbed('', linkThumbnail, name + ' [Video]', link, curPage, p, icon, logo))
+            ems.append(await createEmbed('', linkThumbnail, name + ' [Video]', link, curPage, icon, logo))
 
-        elif postType == 'rich:video': # video, cant rlly embed
+        elif postType == 'rich:video':  # video, cant rlly embed
             linkThumbnail = p.preview['images'][0]['source']['url']
-            
-            ems.append(await createEmbed(p.url, linkThumbnail, name, link, curPage, p, icon, logo))
 
-        elif postType == 'link': # can embed (w/link + thumbnail)
+            ems.append(await createEmbed(p.url, linkThumbnail, name, link, curPage, icon, logo))
+
+        elif postType == 'link':  # can embed (w/link + thumbnail)
             linkThumbnail = p.preview['images'][0]['source']['url']
-            ems.append(await createEmbed(p.url, linkThumbnail, name, link, curPage, p, icon, logo))
+            ems.append(await createEmbed(p.url, linkThumbnail, name, link, curPage, icon, logo))
 
-        elif postType == 'image': # can embed
-            ems.append(await createEmbed('', p.url, name, link, curPage, p, icon, logo))
+        elif postType == 'image':  # can embed
+            ems.append(await createEmbed('', p.url, name, link, curPage, icon, logo))
 
-        else: # PostType is None, could be text or a gallery (or subreddit can make them all Null on purpose, and to which idk what to do about it)
-            if hasattr(p, 'gallery_data'): # gallery or images (embed w/ first pic)
+        else:  # PostType is None, could be text or a gallery (or subreddit can make them all Null on purpose,
+            # and to which idk what to do about it)
+            if hasattr(p, 'gallery_data'):  # gallery or images (embed w/ first pic)
                 if 'caption' in p.gallery_data['items'][0]:
                     caption = p.gallery_data['items'][0]['caption']
                 else:
                     caption = ''
 
-                ems.append(await createEmbed(caption, p.media_metadata[p.gallery_data['items'][0]['media_id']]['s']['u'], name, link, curPage, p, icon, logo))
-            else: # otherwise, just assume its text
-                ems.append(await createEmbed(p.selftext, '', name, link, curPage, p, icon, logo))
-            
+                ems.append(
+                    await createEmbed(caption, p.media_metadata[p.gallery_data['items'][0]['media_id']]['s']['u'], name,
+                                      link, curPage, icon, logo))
+            else:  # otherwise, just assume its text
+                ems.append(await createEmbed(p.selftext, '', name, link, curPage, icon, logo))
+
     return ems
 
 
 class Reddit(commands.Cog):
-    def __init__(self, bot): 
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command(aliases=['r'])
     async def reddit(self, ctx, *args):
-    
+
         icon = ctx.message.author.avatar_url
 
         if len(args) >= 1:
             subreddit = args[0].lower()
         else:
             subreddit = 'spelunky'
-        
+
         if len(args) == 2:
             category = args[1].lower()
         else:
             category = 'hot'
-        
+
         if category not in categories:
             category = 'hot'
-        
+
         posts = await getPosts(subreddit, category, icon)
 
-        if posts == None:
+        if posts is None:
             await ctx.send("Not a valid subreddit! (can't be private)")
 
         m = RedditMenu(posts)
         await m.start(ctx)
-
-        #### failed attempt at own menu, i tried sadge
-        # await message.add_reaction('◀')
-        # await message.add_reaction('▶')
-        
-        # def check(reaction, user):
-        #    return user == ctx.author
-
-        # while True:
-        #     if str(reaction) == '◀':
-        #         if self.curPost > 1:
-        #             self.curPost -= 1
-        #             await message.edit(embed=posts[self.curPost-1])
-        #     elif str(reaction) == '▶':
-        #         if self.curPost < len(posts):
-        #             self.curPost += 1
-        #         await message.edit(embed=posts[self.curPost-1])
-            
-        #     try:
-        #         reaction, user = await self.bot.wait_for('reaction_add', timeout = 30.0, check = check)
-        #     except:
-        #         break
-            #reaction, user = await self.bot.wait_for('reaction_add', timeout = 30.0, check = check)
-            #reaction, user = await self.bot.wait_for('raw_reaction_remove', timeout = 30.0, check = check)
 
 
 class RedditMenu(menus.Menu):
@@ -163,21 +147,21 @@ class RedditMenu(menus.Menu):
         super().__init__()
         self.curPost = 1
         self.posts = posts
-    
+
     async def send_initial_message(self, ctx, channel):
-        if self.posts == None:
+        if self.posts is None:
             return
-        return await ctx.send(embed=self.posts[self.curPost-1])
+        return await ctx.send(embed=self.posts[self.curPost - 1])
 
     @menus.button("◀")
     async def on_arrow_up(self, payload):
         self.curPost -= 1
-        await self.message.edit(embed=self.posts[self.curPost-1])
+        await self.message.edit(embed=self.posts[self.curPost - 1])
 
     @menus.button("▶")
     async def on_arrow_right(self, payload):
         self.curPost += 1
-        await self.message.edit(embed=self.posts[self.curPost-1])
+        await self.message.edit(embed=self.posts[self.curPost - 1])
 
 
 def setup(bot):
